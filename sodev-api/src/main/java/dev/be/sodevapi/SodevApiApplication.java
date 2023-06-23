a62fac1141000a3cd66a964a14a8a3b03e40e7d0
kunsanglee <gosmdochee2@gmail.com>
2023-06-24 03:19:05 +0900

    Add @EntityScan & @EnableJpaRepositories

--- sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
+++ sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
@@ -2,10 +2,14 @@ package dev.be.sodevapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
         scanBasePackages = { "dev.be.sodevapi", "dev.be.sodevcommon" }
 )
+@EntityScan(basePackages = "dev.be.sodevcommon.domain")
+@EnableJpaRepositories(basePackages = "dev.be.sodevcommon.repository")
 public class SodevApiApplication {
 
     public static void main(String[] args) {
