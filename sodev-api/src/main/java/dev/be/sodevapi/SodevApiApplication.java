bc5a5038cb6f7986d726a69d25cab658b3f50b78
kunsanglee <gosmdochee2@gmail.com>
2023-06-26 01:42:35 +0900

    SodevApiApplication.java 수정
    `@EntityScan`의 basePackages 에 sodev-common의 entity가 들어있는 패키지 구조가 변경되어서 entity 디렉토리의 상위 디렉토리인 domain으로 지정했다.
    
    `@EnableJpaAuditing` 추가하여 BaseEntity의 @EntityListeners(AuditingEntityListener.class) 를 인식할 수 있게 추가했다.

--- sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
+++ sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
@@ -3,11 +3,13 @@ package dev.be.sodevapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan(basePackages = "dev.be.sodevcommon.domain")
+@EnableJpaAuditing
+@EntityScan(basePackages = "dev.be.sodevcommon.model")
 @EnableJpaRepositories(basePackages = "dev.be.sodevcommon.repository")
+@SpringBootApplication
 public class SodevApiApplication {
 
     public static void main(String[] args) {
