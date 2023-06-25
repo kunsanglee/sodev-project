286909aa99178047e1f9052996c5f40ad2ec933e
kunsanglee <gosmdochee2@gmail.com>
2023-06-26 00:33:27 +0900

    yml 분리, build.gradle 통합 및 모듈별 수정
    yml은 application.yml에서 공통 설정을 하고
    local, dev, prod 각각에 맞게 yml 파일을 분리했다.
    
    루트 패키지에 속하는  build.gradle 안에 공통 설정을 하고
    api, common 모듈에 각각 필요한 의존성만 관리하도록 수정.

--- sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
+++ sodev-api/src/main/java/dev/be/sodevapi/SodevApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(
-        scanBasePackages = { "dev.be.sodevapi", "dev.be.sodevcommon" }
-)
+@SpringBootApplication
 @EntityScan(basePackages = "dev.be.sodevcommon.domain")
 @EnableJpaRepositories(basePackages = "dev.be.sodevcommon.repository")
 public class SodevApiApplication {
