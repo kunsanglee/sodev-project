962bca3dc002f701dbaec952213701fdd3dfc7fd
kunsanglee <gosmdochee2@gmail.com>
2023-07-02 19:27:34 +0900

    #09 커밋에서 필요없는 클래스 제거
    build 관련하여 오류 수정, 더이상 사용하지 않는 이전의 시큐리티와 관련 설정, 디렉토리 구조 수정

--- src/main/java/dev/sodev/config/JpaAuditingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package dev.sodev.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfiguration {
-}
