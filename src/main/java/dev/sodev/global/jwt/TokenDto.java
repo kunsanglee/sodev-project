2c56a62b2bd30bf93efd4e48863419850c151c93
kunsanglee <gosmdochee2@gmail.com>
2023-07-18 16:05:31 +0900

    #25 기존 JWT 관련 삭제 및 캐싱 수정
    기존에 구현했던 JWT 클래스를 전면 수정하고, 회원정보를 redis 에 캐싱하였던 것이 바람직하지 않다고 생각하여 삭제했다.
    또 이밖에 불필요한 것들을 삭제하였다.

--- src/main/java/dev/sodev/global/jwt/TokenDto.java
+++
@@ -1,9 +0,0 @@
-package dev.sodev.global.jwt;
-
-
-public record TokenDto(String accessToken, String refreshToken) {
-    public TokenDto(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
-}
