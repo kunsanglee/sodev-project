656b1746606bb4fc0ee9f11dce5f600c2e95d00d
kunsanglee <gosmdochee2@gmail.com>
2023-07-18 16:20:40 +0900

    #25 JWT, Security 관련 클래스 새로 작성
    기존에 작성한 jwt, security 클래스가 너무 복잡하고 불필요한 부분이 많아서 전면 수정하였다.
    
    * AuthController
    로그인, 토큰 재발급, 로그아웃, 회원탈퇴의 경우에는 MemberController 에 있는 것 보다는 인증에 관련된 요청이라고 생각하여 AuthController 에서 받는걸로 수정하였다.
    
    *JwtAuthenticationFilter
    request -> jwtAuthenticationFilter 로 거치는데 어떤 요청이든 redis 안에 블랙리스트로 지정한 access, refresh 토큰을 가지고있으면 적절하게 에러처리가 되도록 작성하였다.
    JwtSecurityConfig 에 LogoutFilter 이후에 동작하도록 클래스를 작성하고 SecurityConfig에 apply 하였다.
    
    * JwtAuthenticationProvider
    JwtAuthenticationFilter -> JwtAuthenticationProvider 로 진행될때 Authentication 을 받아서 클래임을 생성하여 반환하는데, 이 과정에 토큰의 유효성을 검사하여 이상이 없을 경우 클래임을 반환한다.
    
    * JwtInvalidException
    Jwt 관련하여 에러가 발생할 경우 감쌀 수 있는 Exception을 작성하였는데, AuthenticationException 을 상속받아 위임하였다.
    
    * JsonWebTokenIssuer
    secretKey 와 refreshSecretKey, expireMin, refreshExpireMin 을 각각 가지고 토큰을 발급, 클래임 추출, 토큰의 유효기간을 얻는 메서드를 정의하고 다른 클래스에서 사용할 수 있도록 하였다.
    
    * JwtAuthenticationToken
    AbstractAuthenticationToken 을 상속받아서 구현하였다.
    
    * AuthService
    로그인, 재발급, 로그아웃, 회원탈퇴 인증 인가에 관련한 AuthController 에서 들어오는 요청들을 처리하는 서비스 클래스를 작성하였다.

---
+++ src/main/java/dev/sodev/global/security/dto/JsonWebTokenDto.java
@@ -0,0 +1,7 @@
+package dev.sodev.global.security.dto;
+
+import lombok.Builder;
+
+@Builder
+public record JsonWebTokenDto(String grantType, String accessToken, String refreshToken) {
+}
---
+++ src/main/java/dev/sodev/global/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package dev.sodev.global.security.filter;
+
+import dev.sodev.global.exception.ErrorCode;
+import dev.sodev.global.exception.SodevApplicationException;
+import dev.sodev.global.redis.RedisService;
+import dev.sodev.global.security.tokens.JwtAuthenticationToken;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final AuthenticationManager authenticationManager;
+    private final RedisService redisService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, RedisService redisService) {
+        this.authenticationManager = authenticationManager;
+        this.redisService = redisService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String jwt = resolveToken(request);
+
+        log.info("doFilter 들어옴");
+
+        if (StringUtils.hasText(jwt)) {
+
+            if (redisService.hasKey(jwt)) { // 토큰이 블랙리스트(로그아웃, 회원탈퇴)에 있어서 재로그인 요청
+                log.info("블랙리스트에 담긴 토큰 로직 실행");
+                String value = (String) redisService.get(jwt);
+                if (value.equals("logout")) {
+                    log.info("로그아웃한 회원의 토큰 -> 재로그인 요청");
+                    throw new SodevApplicationException(ErrorCode.ACCESS_UNAUTHORIZED); // 로그아웃한 토큰 -> 재로그인
+                } else {
+                    log.info("탈퇴한 회원의 토큰 -> 재가입 요청");
+                    throw new SodevApplicationException(ErrorCode.WITHDRAWAL_USER); // 탈퇴한 회원의 토큰 -> 재가입
+                }
+            }
+
+            try {
+                Authentication jwtAuthenticationToken = new JwtAuthenticationToken(jwt);
+                Authentication authentication = authenticationManager.authenticate(jwtAuthenticationToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (AuthenticationException authenticationException) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
