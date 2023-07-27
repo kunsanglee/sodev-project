714863f90a679662be1e1056b0173295058b2de2
kunsanglee <gosmdochee2@gmail.com>
2023-07-28 01:47:08 +0900

    #31 팔로잉 팔로우
    MemberInfo를 조회할때 해당 회원의 팔로잉, 팔로워를 같이 가져오는데, Follow 엔티티를 그대로 노출하지 않고 Dto로 변환하여 가져올 수 있도록 FollowDto를 작성하여 Follow 엔티티를 변환하는 메서드를 추가하였다.

--- src/main/java/dev/sodev/domain/follow/dto/FollowDto.java
+++ src/main/java/dev/sodev/domain/follow/dto/FollowDto.java
@@ -1,7 +1,24 @@
 package dev.sodev.domain.follow.dto;
 
+import dev.sodev.domain.follow.Follow;
 import lombok.Builder;
 
 @Builder
 public record FollowDto(Long memberId, String email, String nickName) {
+
+    public static FollowDto follower(Follow follower) {
+        return FollowDto.builder()
+                .memberId(follower.getFromMember().getId())
+                .email(follower.getFromMember().getEmail())
+                .nickName(follower.getFromMember().getNickName())
+                .build();
+    }
+
+    public static FollowDto following(Follow following) {
+        return FollowDto.builder()
+                .memberId(following.getToMember().getId())
+                .email(following.getToMember().getEmail())
+                .nickName(following.getToMember().getNickName())
+                .build();
+    }
 }
