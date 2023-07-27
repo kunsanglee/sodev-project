4e7c4db1564fa6e9565ef206564480de3e55d895
kunsanglee <gosmdochee2@gmail.com>
2023-07-28 01:49:15 +0900

    #31 좋아요 관련 수정
    * LikeDto
    Likes 엔티티를 노출하지 않기 위해 Dto를 작성하였다.
    
    * LikeCustomRepository, LikeCustomRepositoryImpl
    프로젝트 피드에 있는 좋아요 목록을 모두 가져오는 메서드를 작성하였다.

--- src/main/java/dev/sodev/domain/likes/repository/LikeCustomRepositoryImpl.java
+++ src/main/java/dev/sodev/domain/likes/repository/LikeCustomRepositoryImpl.java
@@ -1,13 +1,20 @@
 package dev.sodev.domain.likes.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import dev.sodev.domain.likes.Likes;
+import dev.sodev.domain.likes.QLikes;
+import dev.sodev.domain.likes.dto.LikesDto;
+import dev.sodev.domain.member.QMember;
 import jakarta.persistence.EntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static dev.sodev.domain.likes.QLikes.*;
+import static dev.sodev.domain.member.QMember.member;
 
 
 @Repository
@@ -29,5 +36,14 @@ public class LikeCustomRepositoryImpl implements LikeCustomRepository{
         return queryFactory.selectFrom(likes)
                 .where(likes.member.id.eq(id).and(likes.project.id.eq(projectId)))
                 .fetchOne();
+    }
+
+    @Override
+    public List<LikesDto> likeList(Long projectId) {
+        return queryFactory.select(Projections.constructor(LikesDto.class, likes.id, member.id, member.nickName))
+                .from(likes)
+                .join(likes.member, member)
+                .where(likes.project.id.eq(projectId))
+                .fetch();
     }
 }
