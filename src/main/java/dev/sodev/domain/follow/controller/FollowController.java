c0e3c085f4332d1de86b7eb291d7cee11de1a79b
kunsanglee <gosmdochee2@gmail.com>
2023-07-30 01:42:59 +0900

    #33 Follow 수정
    회원 조회 요청시 팔로워 팔로잉 목록을 함께 반환했었는데,
    조회 요청 자체가 너무 무거워지는 것을 방지하고자 팔로워, 팔로잉 숫자를 보여주고, 직접 목록을 조회하는 요청을 따로 했을때 리스트를 반환하도록 분리했다.

--- src/main/java/dev/sodev/domain/follow/controller/FollowController.java
+++ src/main/java/dev/sodev/domain/follow/controller/FollowController.java
@@ -1,5 +1,7 @@
 package dev.sodev.domain.follow.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import dev.sodev.domain.follow.dto.FollowDto;
 import dev.sodev.domain.follow.dto.FollowRequest;
 import dev.sodev.domain.follow.dto.FollowResponse;
@@ -7,6 +9,9 @@ import dev.sodev.domain.follow.service.FollowService;
 import dev.sodev.global.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+
+
 
 import java.util.List;
 
@@ -29,27 +34,19 @@ public class FollowController {
         return Response.success(response);
     }
 
-    @GetMapping("/follower")
-    public Response<FollowResponse<List<FollowDto>>> getFollowers() {
-        FollowResponse<List<FollowDto>> response = followService.getFollowers();
-        return Response.success(response);
-    }
-
-    @GetMapping("/following")
-    public Response<FollowResponse<List<FollowDto>>> getFollowing() {
-        FollowResponse<List<FollowDto>> response = followService.getFollowing();
-        return Response.success(response);
-    }
-
+    /**
+     * 기존에 있던 본인의 팔로워, 팔로잉 목록도 @PathVariable id 입력해주는게
+     * 메서드가 중복되지 않고 깔끔할 것 같아서 제거하고 타 회원 조회할 때 사용하는 api 로 같이 처리.
+     */
     @GetMapping("{id}/follower")
-    public Response<FollowResponse<List<FollowDto>>> getMembersFollowers(@PathVariable Long id) {
-        FollowResponse<List<FollowDto>> response = followService.getFollowers();
+    public Response<Slice<FollowDto>> getMembersFollowers(@PathVariable Long id, Pageable pageable) {
+        Slice<FollowDto> response = followService.getFollowerByMemberId(id, pageable);
         return Response.success(response);
     }
 
     @GetMapping("{id}/following")
-    public Response<FollowResponse<List<FollowDto>>> getMembersFollowing(@PathVariable Long id) {
-        FollowResponse<List<FollowDto>> response = followService.getFollowing();
+    public Response<Slice<FollowDto>> getMembersFollowing(@PathVariable Long id, Pageable pageable) {
+        Slice<FollowDto> response = followService.getFollowingByMemberId(id, pageable);
         return Response.success(response);
     }
 }
--- src/main/java/dev/sodev/domain/follow/repository/FollowRepository.java
+++ src/main/java/dev/sodev/domain/follow/repository/FollowRepository.java
@@ -1,5 +1,7 @@
 package dev.sodev.domain.follow.repository;
 
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Pageable;
 import dev.sodev.domain.follow.Follow;
 import dev.sodev.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,12 +10,16 @@ import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    List<Follow> findAllByFromMember(Member fromMember);
-
-    List<Follow> findAllByToMember(Member toMember);
-
-    void deleteByFromMemberAndToMember(Member fromMember, Member toMember);
+//    List<Follow> findAllByFromMember(Member fromMember);
+//
+//    List<Follow> findAllByToMember(Member toMember);
+//
+//    void deleteByFromMemberAndToMember(Member fromMember, Member toMember);
 
     Follow findByFromMemberAndToMember(Member fromMember, Member toMember);
+
+    Slice<Follow> findAllByToMember_Id(Long memberId, Pageable pageable); // 팔로워 조회
+
+    Slice<Follow> findAllByFromMember_Id(Long memberId, Pageable pageable); // 팔로잉 조회
 
 }
