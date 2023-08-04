ebc0855f8018ab1aa763f1036ac60a813f13ed77
kunsanglee <gosmdochee2@gmail.com>
2023-08-05 00:22:26 +0900

    #37 댓글 관련 수정
    * CommentRepository, CommentServiceImpl 
    CommentRepository -> CommentCustomRepository 구현하도록 하여 Service 단에서 CommentRepository 인터페이스에만 의존하도록 수정함.

--- src/main/java/dev/sodev/domain/comment/repsitory/CommentRepository.java
+++ src/main/java/dev/sodev/domain/comment/repsitory/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentCustomRepository {
 
     List<Comment> findAllByMemberEmail(String memberEmail);
     List<Comment> findAllByMemberId(Long id);
