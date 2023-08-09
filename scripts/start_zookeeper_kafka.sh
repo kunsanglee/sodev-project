972606934e6f3dbeb13ba4874787d90e4f1475e3
kunsanglee <gosmdochee2@gmail.com>
2023-08-09 17:23:52 +0900

    #fix start_zookeeper_kafka.sh
    daemon으로 실행하게 수정

--- scripts/start_zookeeper_kafka.sh
+++ scripts/start_zookeeper_kafka.sh
@@ -2,12 +2,12 @@
 
 # Zookeeper 프로세스 확인
 if ! pgrep -f "zookeeper-server-start" > /dev/null; then
-    /home/ubuntu/kafka/bin/zookeeper-server-start.sh /home/ubuntu/kafka/config/zookeeper.properties &
+    /home/ubuntu/kafka/bin/zookeeper-server-start.sh -daemon /home/ubuntu/kafka/config/zookeeper.properties
 fi
 
 sleep 20  # Zookeeper가 완전히 시작되길 기다린 후
 
 # Kafka 프로세스 확인
 if ! pgrep -f "kafka-server-start" > /dev/null; then
-    /home/ubuntu/kafka/bin/kafka-server-start.sh /home/ubuntu/kafka/config/server.properties &
+    /home/ubuntu/kafka/bin/kafka-server-start.sh -daemon /home/ubuntu/kafka/config/server.properties
 fi
