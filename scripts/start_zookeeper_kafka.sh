60c6ba77e8943a040fb1830e005f22a7ee789de0
kunsanglee <gosmdochee2@gmail.com>
2023-08-09 16:12:45 +0900

    #fix start_zookeeper_kafka.sh
    주키퍼와 카프카가 구동하고있지 않은 상태에서만 실행하도록 함.
    sleep 10 -> 20 초로 늘림.

--- scripts/start_zookeeper_kafka.sh
+++ scripts/start_zookeeper_kafka.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-/home/ubuntu/kafka/bin/zookeeper-server-start.sh /home/ubuntu/kafka/config/zookeeper.properties &
+# Zookeeper 프로세스 확인
+if ! pgrep -f "zookeeper-server-start" > /dev/null; then
+    /home/ubuntu/kafka/bin/zookeeper-server-start.sh /home/ubuntu/kafka/config/zookeeper.properties &
+fi
 
-sleep 10  # Zookeeper가 완전히 시작되길 기다린 후
+sleep 20  # Zookeeper가 완전히 시작되길 기다린 후
 
-/home/ubuntu/kafka/bin/kafka-server-start.sh /home/ubuntu/kafka/config/server.properties &
+# Kafka 프로세스 확인
+if ! pgrep -f "kafka-server-start" > /dev/null; then
+    /home/ubuntu/kafka/bin/kafka-server-start.sh /home/ubuntu/kafka/config/server.properties &
+fi/ubuntu/kafka/config/server.properties &
