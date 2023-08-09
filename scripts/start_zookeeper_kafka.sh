8428a0ce814e9f7618403bbea9780ef329c0e56d
kunsanglee <gosmdochee2@gmail.com>
2023-08-09 16:16:58 +0900

    #fix start_zookeeper_kafka.sh
    오타 수정

--- scripts/start_zookeeper_kafka.sh
+++ scripts/start_zookeeper_kafka.sh
@@ -10,4 +10,4 @@ sleep 20  # Zookeeper가 완전히 시작되길 기다린 후
 # Kafka 프로세스 확인
 if ! pgrep -f "kafka-server-start" > /dev/null; then
     /home/ubuntu/kafka/bin/kafka-server-start.sh /home/ubuntu/kafka/config/server.properties &
-fi/ubuntu/kafka/config/server.properties &
+fi
